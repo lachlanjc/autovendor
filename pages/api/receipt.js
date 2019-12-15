db52a10abef8babe9fedfe2922057a31e0445124
Glitch (autovendor) <none>
2019-12-15 00:44:31 +0000

    Fix timezone math

--- pages/api/receipt.js
+++ pages/api/receipt.js
@@ -7,15 +7,17 @@ export default (req, res) => {
   const { to, text } = req.query
   console.log('SENDING', to, text)
   if (!to) res.status(422).json({ error: 'Missing to phone number' })
+
   const dt = new Date()
-  dt.setMinutes(dt.getMinutes() + dt.getTimezoneOffset() - 300)
+  dt.setTime(dt.getTime() + dt.getTimezoneOffset() * 60 * 1000)
+  const et = new Date(dt.getTime() + -300 * 60 * 1000)
 
   const content = text
     ? `“${text}”
 `
     : ''
   const body = `AUTOVENDOR @ NYU IMA
-${content}${dt.toLocaleString()}`
+${content}${et.toLocaleString()}`
 
   twilio.messages
     .create({ body, to: `+1${to}`, from: '+18649736177' })
