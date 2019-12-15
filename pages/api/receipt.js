21924f6ecebd263a01c15716212fc9476ef06729
Lachlan Campbell <lachlan@hackclub.com>
2019-12-15 01:29:45 -0500

    Add secrets to now.json

--- pages/api/receipt.js
+++ pages/api/receipt.js
@@ -1,7 +1,7 @@
-// const twilio = require('twilio')(
-//   'ACd2f0d2761422ef7e43d3de6a6a388e9f',
-//   process.env.TWILIO_AUTH
-// )
+const twilio = require('twilio')(
+  'ACd2f0d2761422ef7e43d3de6a6a388e9f',
+  process.env.TWILIO_AUTH
+)
 
 export default (req, res) => {
   const { to, text } = req.query
@@ -12,15 +12,14 @@ export default (req, res) => {
 ${text ? `“${text}”
 ` : ''}Dec 15, 2019`
   
-  // twilio.messages
-  //   .create({ body, to: `+1${to}`, from: '+18649736177' })
-  //   .then(message => {
-  //     const { date_sent, status } = message
-  //     res.json({ date_sent, status, body })
-  //   })
-  //   .catch(error => {
-  //     console.error(error)
-  //     res.status(422).json({ error: error.message })
-  //   })
-  res.json({ body })
+  twilio.messages
+    .create({ body, to: `+1${to}`, from: '+18649736177' })
+    .then(message => {
+      const { date_sent, status } = message
+      res.json({ date_sent, status, body })
+    })
+    .catch(error => {
+      console.error(error)
+      res.status(422).json({ error: error.message })
+    })
 }
